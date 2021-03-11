357bfc16f7430fa290191ef8c682da4e36156cb6
drlazar <drlazar@users.noreply.github.com>
2021-03-10 17:51:28 -0800

    Project Folders Added

---
+++ Kovaak 3.0/Assets/Receiver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Receiver : MonoBehaviour
+{
+    public Text totalHits;
+    public int hits = 0;
+
+    private void Start()
+    {
+        this.transform.position = new Vector3(-36.71f, 1.5f, 3);
+    }
+
+    public void Receive()
+    {
+        hits++;
+        totalHits.text = hits.ToString();
+        this.gameObject.GetComponent<AudioSource>().Play();
+        gameObject.SetActive(false);
+        this.transform.position = new Vector3(-36.71f, Random.Range(-1.5f, 4.5f), Random.Range(-1.1f, 6.9f));
+        gameObject.SetActive(true);
+    }
+}
---
+++ Kovaak 3.0/Assets/Shoot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shoot : MonoBehaviour
+{
+    public Text totalShots;
+    public Text accuracy;
+    public GameObject target;
+    int shots = 0;
+    float percentage = 0f;
+    bool checkRaycast;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalShots.text = shots.ToString();
+        Receiver receive = target.GetComponent<Receiver>();
+        percentage = ((float)receive.hits / (float)shots) * 100;
+        accuracy.text = "Accuracy: " + percentage.ToString() + "%";
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            shots++;
+            RaycastHit hit;
+            checkRaycast = Physics.Raycast(this.transform.position, this.transform.TransformDirection(Vector3.forward), out hit);
+            if (checkRaycast)
+            {
+                hit.transform.SendMessage("Receive", SendMessageOptions.DontRequireReceiver);
+            }
+            else
+            {
+                this.GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}
